31e6612d9b93e4e54955b8aa5139b49f00a426ae
Noah Johnson <noah.johnson@loupmail.com>
2022-02-14 15:22:21 -0600

    Update to support new index format

--- canto_config.pl
+++ canto_config.pl
@@ -13,7 +13,11 @@ canto_config.pl - Compile a JavaScript configuration file for Cantotype.
 
 =head1 SYNOPSIS
 
-  canto_config.pl /url/to/index.gz /url/to/sql/ > cantotype_config.js
+  canto_config.pl
+    /url/to/data/
+    cantotype_data_index.gz
+    /url/to/sql/
+      > cantotype_config.js
 
 =head1 DESCRIPTION
 
@@ -21,12 +25,14 @@ Cantotype needs a JavaScript configuration file that is specific to the
 installation to run.  This Perl script will generate the necessary
 configuration file.
 
-Only two parameters are needed.  The first parameter is the URL to the
-Cantotype data index file.  This is a GZip file that stores a JSON
-object that defines where all the data files that need to be loaded into
-the database should be downloaded from.
+Only three parameters are needed.  The first parameter is the URL to the
+server directory storing the Cantotype data files.  If this is to a
+folder, it should end with a slash character.  The second parameter is
+the filename of the data index file within that data folder.  All
+filenames referenced from that data index file will be assumed to be in
+the same directory.
 
-The second parameter is used during the initialization of sql.js to
+The third parameter is used during the initialization of sql.js to
 define the locateFile function, which locates files needed by sql.js.
 The locateFile function will take the filename given as input and prefix
 this parameter value to it to form the returned URL.
@@ -38,9 +44,9 @@ initialization.
 
 The configuration file is a JavaScript source file that defines a global
 variable named C<canto_config>.  This variable is a JavaScript object
-that has properties C<data_index_url> and C<sql_file_base> corresponding
-to the URL of the data index file and the URL prefix for SQL files,
-respectively.
+that has properties C<data_base>, C<index_name>, and C<sql_base>
+corresponding to the URL prefix for the data files, the name of the data
+file index, and the URL prefix for SQL files, respectively.
 
 =cut
 
@@ -48,21 +54,23 @@ respectively.
 # Program entrypoint
 # ==================
 
-# Check that we got two parameters
+# Check that we got three parameters
 #
-($#ARGV == 1) or die "Wrong number of parameters, stopped";
+($#ARGV == 2) or die "Wrong number of parameters, stopped";
 
 # Store the parameters
 #
-my $arg_index = $ARGV[0];
-my $arg_sql   = $ARGV[1];
+my $arg_data  = $ARGV[0];
+my $arg_index = $ARGV[1];
+my $arg_sql   = $ARGV[2];
 
 # Generate the configuration object
 #
 my %config_obj;
 
-$config_obj{"data_index_url"} = "$arg_index";
-$config_obj{"sql_file_base"}  = "$arg_sql";
+$config_obj{"data_base"} = "$arg_data";
+$config_obj{"index_name"} = "$arg_index";
+$config_obj{"sql_base"}  = "$arg_sql";
 
 # Generate the JSON for the configuration object
 #
