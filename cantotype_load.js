407d04a4d449a9aaf19484e65051448d092f4a88
Noah Johnson <noah.johnson@loupmail.com>
2022-02-17 16:02:48 -0600

    Begin new cache architecture

--- cantotype_load.js
+++ cantotype_load.js
@@ -62,16 +62,18 @@
    * 
    * This function returns immediately.  The download and load is done
    * asynchronously.  If the process succeeds, f_done() is called with a
-   * single parameter that stores the result of JSON.parse() on the
-   * decompressed file data.  If there is an error, f_err() is called
-   * with a single parameter that stores the exception that occurred.
+   * parameter that stores the result of JSON.parse() on the inflated
+   * file data, and a second parameter that stores the raw data of the
+   * compressed index file in an array buffer.  If there is an error,
+   * f_err() is called with a single parameter that stores the exception
+   * that occurred.
    * 
    * Parameters:
    * 
    *   src : string - the URL of the GZip file to load
    * 
-   *   f_done : function - callback that takes a single argument which
-   *   is the parsed JSON result
+   *   f_done : function - callback that takes a the parsed JSON result
+   *   and the raw compressed data
    * 
    *   f_err : function - callback that takes a single argument which is
    *   the exception that caused the operation to fail
@@ -127,7 +129,7 @@
       
           // If we got here, we finally have the parsed JSON, so invoke
           // the done callback
-          f_done(js);
+          f_done(js, raw_data);
           
         } else {
           // File request failed
@@ -142,254 +144,224 @@
   }
   
   /*
+   * Verify that the parsed representation of a data file index is
+   * valid.
+   * 
+   * Parameters:
+   * 
+   *   js - the parsed representation to verify
+   * 
+   * Return:
+   * 
+   *   true if valid, false if not
+   */
+  function verifyIndex(js) {
+    // @@TODO:
+    return true;
+  }
+  
+  /*
+   * Attempt to connect to the IndexedDB with the appropriate version.
+   * 
+   * If this works out, f_db is called with the opened database handle.
+   * 
+   * If there is any problem, f_nodb is called with no parameter.
+   * 
+   * Parameters:
+   * 
+   *   f_db : function - callback that takes an IndexedDB handle if we
+   *   opened the database
+   * 
+   *   f_nodb : function - callback that takes nothing, called if we
+   *   couldn't connect to IndexedDB
+   */
+  function connectIXDB(f_db, f_nodb) {
+    // @@TODO:
+    f_nodb();
+  }
+  
+  /*
+   * Given a database connection to IndexedDB and a parsed
+   * representation of the data index file that has all binary data
+   * within it, as well as the raw byte buffer of the compressed data
+   * index file, update the IndexedDB cache if necessary.
+   * 
+   * This returns quickly, and errors are only noted on the console.
+   * Since the cache is not required for program operation, the main
+   * Cantotype program does not need to wait for the update to complete.
+   * 
+   * Parameters:
+   * 
+   *   db : the database connection
+   * 
+   *   jsi : the parsed representation with data files within it
+   * 
+   *   raw_index : the raw array buffer of the compressed data index
+   */
+  function syncCache(db, jsi, raw_index) {
+    // @@TODO:
+  }
+  
+  /*
+   * Perform the first pass over the data file index to load any data
+   * that has already been cached.
+   * 
+   * db is assumed to be a connection to the Cantotype IndexedDB
+   * database.  jsi is assumed to be the parsed representation of the
+   * data file index.
+   * 
+   * While working, status reports may be sent to f_status with a string
+   * parameter.
+   * 
+   * f_done is called with no parameters if the pass completes
+   * successfully.  Cached data may have been added into the passed data
+   * file index in this case.
+   * 
+   * f_err is called if the pass fails, with a reason parameter.
+   * 
+   * Parameters:
+   *
+   *   jsi : the parsed data file index
+   *  
+   *   f_done : function called when pass is complete
+   * 
+   *   f_status : function called to report loading progress with a
+   *   string parameter holding the current loading status
+   * 
+   *   f_err : function called to report error with a reason parameter
+   */
+  function dataPassOne(db, jsi, f_done, f_status, f_err) {
+    // @@TODO:
+    f_done();
+  }
+  
+  /*
+   * Perform the second pass over the data file index to fetch missing
+   * data over HTTP.
+   * 
+   * jsi is assumed to be the parsed representation of the data file
+   * index, which may already have some data loaded into it.
+   * 
+   * While working, status reports may be sent to f_status with a string
+   * parameter.
+   * 
+   * f_done is called with no parameters if the pass completes
+   * successfully.  All data will have been added into the passed data
+   * file index in this case.
+   * 
+   * f_err is called if the pass fails, with a reason parameter.
+   * 
+   * Parameters:
+   *
+   *   jsi : the parsed data file index
+   *  
+   *   f_done : function called when pass is complete
+   * 
+   *   f_status : function called to report loading progress with a
+   *   string parameter holding the current loading status
+   * 
+   *   f_err : function called to report error with a reason parameter
+   */
+  function dataPassTwo(jsi, f_done, f_status, f_err) {
+    // @@TODO:
+    f_done();
+  }
+  
+  /*
    * Public functions
    * ================
    */
   
   /*
-   * Asynchronously load all the character and dictionary data records
-   * from the Cantotype data files.
+   * Asynchronously load all database files from HTTP and the IndexedDB
+   * cache, and update the IndexedDB cache if possible.
    * 
-   * This function will return immediately.  All loading will be done
-   * through the given callback functions.
+   * While loading is in progress, the f_status callback may be called
+   * with a string containing a progress report that can be shown to the
+   * user to indicate how far along the loading process is.
    * 
-   * Records are loaded in blocks corresponding to how records are split
-   * across source data files.  Whenever a block of character records
-   * are ready, the f_ce function will be invoked with the parsed
-   * representation of those records, a callback to invoke when done,
-   * and a callback to invoke in case or error, which takes a reason.
-   * 
-   * Whenever a word record is ready, the f_we function will be called,
-   * which has the same interface as the f_ce function.
-   * 
-   * See the canto_compile.pl script for further information about the
-   * data format within these data arrays.
-   * 
-   * Both the f_ce and f_we functions may throw exceptions.  If an
-   * exception is thrown, it will be caught and then passed to the f_err
-   * callback as a parameter.
-   * 
-   * If the function manages asynchronously to report all the data
-   * records to the f_ce and f_we callbacks, and all of those callbacks
-   * invoke the done callback without throwing an exception, then at the
-   * end of the operation, the f_done callback will be called to
-   * indicate the loading operation is complete.
-   * 
-   * If any of the f_ce or f_we callbacks throw an exception, or if they
-   * call the error callback, or if there is any kind of error while
-   * asynchronously processing the data, the f_err callback will be
-   * called with the exception as an argument.
-   * 
-   * Eventually, either the f_done or f_err callback will be invoked to
-   * indicate the end of the asynchronous loading operation.
+   * If loading finishes successfully, f_ready will be called with no
+   * parameters.  If there is any failure, f_err will be called with a
+   * reason parameter.
    * 
    * Parameters:
    * 
-   *   f_ce : function - callback function that takes a array parameter
-   *   of character records to process, a "done" callback function that
-   *   has no arguments, and an "error" callback function that has a
-   *   single reason argument
+   *   f_ready : function called when load is complete
    * 
-   *   f_we : function - callback function that takes a array parameter
-   *   of word records to process, a "done" callback function that has
-   *   no arguments, and an "error" callback function that has a single
-   *   reason argument
+   *   f_status : function called to report loading progress with a
+   *   string parameter holding the current loading status
    * 
-   *   f_done : function - callback function invoked when all records
-   *   have successfully been reported through the f_ce and f_we
-   *   callbacks
-   * 
-   *   f_err : function - callback function invoked with a single
-   *   parameter of an exception object when any of the f_ce or f_we
-   *   callbacks throws an exception, or when there is an error during
-   *   asynchronous processing
+   *   f_err : function called to report loading error with a reason
+   *   parameter
    */
-  function go(f_ce, f_we, f_done, f_err) {
+  function initDB(f_ready, f_status, f_err) {
     
-    var func_name = "go";
+    var func_name = "initDB";
     
     // Check parameters
-    if ((typeof(f_ce) !== "function") ||
-        (typeof(f_we) !== "function") ||
-        (typeof(f_done) !== "function") ||
+    if ((typeof(f_ready) !== "function") ||
+        (typeof(f_status) !== "function") ||
         (typeof(f_err) !== "function")) {
       fault(func_name, 100);
     }
     
-    // First we want to load the index file, function continues
-    // asynchronously in the callback
-    loadGZJSON(
-        canto_config.data_base + canto_config.index_name,
-        function(js_index) {
-      
-      var ca, i, f;
-      
-      // Wrap handling in a try-catch that reports to the f_err callback
-      // if there is any exception
-      try {
-        // Parsed JSON should be object
-        if (typeof(js_index) !== "object") {
-          fault(func_name, 200);
-        }
-
-        // Parse JSON should contain "charlist" and "wordlist"
-        // properties
-        if (!(("charlist" in js_index) && ("wordlist" in js_index))) {
-          fault(func_name, 210);
+    // First we need to get and parse the data index file from HTTP
+    loadGZJSON(canto_config.data_base + canto_config.index_name,
+      function(js_index, raw_index) {
+        
+        // Verify the index file
+        if (!verifyIndex(js_index)) {
+          f_err("Data file index not valid");
+          return;
         }
         
-        // charlist and wordlist properties must be arrays
-        if (!((js_index.charlist instanceof Array) &&
-              (js_index.wordlist instanceof Array))) {
-          fault(func_name, 220);
-        }
-        
-        // Build a combined array that will have a two-element subarray
-        // for each element; the first element in each subarray is a URL
-        // and the second element in each subarray is a string that is
-        // either "word" or "char" for the type of data file to process
-        ca = [];
-        
-        for(i = 0; i < js_index.charlist.length; i++) {
-          if (typeof(js_index.charlist[i]) !== "string") {
-            fault(func_name, 230);
+        // Now attempt a connection to IndexedDB
+        connectIXDB(
+          function(db) {
+            // We got IndexedDB open, so perform the first pass over the
+            // data index to load any locally cached data
+            dataPassOne(db, js_index,
+              function() {
+                // Now perform the second pass to fetch any missing data
+                // over HTTP
+                dataPassTwo(js_index,
+                  function() {
+                    // Finally, we'll try to update the cached data in
+                    // the IndexedDB; this isn't strictly necessary for
+                    // the program, so we can let this update run
+                    // asynchronously without waiting for it to complete
+                    // and without checking for errors
+                    try {
+                      syncCache(db, js_index, raw_index);
+                    } catch (exb) {
+                      console.log("Failed to syncCache.");
+                    }
+                    
+                    // We can now invoke the completion callback
+                    f_ready();
+                    
+                  },
+                  f_status, f_err
+                );
+                
+              },
+              f_status, f_err
+            );
+            
+          },
+          function() {
+            // IndexedDB not available, so we just perform the second
+            // pass to load everything
+            dataPassTwo(js_index, f_ready, f_status, f_err);
           }
-          ca.push([
-            canto_config.data_base + js_index.charlist[i],
-            "char"
-          ]);
-        }
+        );
         
-        for(i = 0; i < js_index.wordlist.length; i++) {
-          if (typeof(js_index.wordlist[i]) !== "string") {
-            fault(func_name, 240);
-          }
-          ca.push([
-            canto_config.data_base + js_index.wordlist[i],
-            "word"
-          ]);
-        }
-        
-        // Process data files
-        i = 0;
-        f = function() {
-          // If i is past the end of the combined array, invoke the done
-          // handler and leave
-          if (i >= ca.length) {
-            f_done();
-            return;
-          }
-          
-          // Not past the end, so asynchronously load this data file
-          loadGZJSON(
-            ca[i][0],
-            function(js_data) {
-              
-              // Invoke the proper handler function for this block
-              if (ca[i][1] === "char") {
-                // Increment i so we process the next data file next
-                // time
-                i++;
-                
-                // Invoke the character data handler
-                f_ce(js_data, f, f_err);
-                
-              } else if (ca[i][1] === "word") {
-                // Increment i so we process the next data file next
-                // time
-                i++;
-                
-                // Invoke the word data handler
-                f_we(js_data, f, f_err);
-                
-              } else {
-                // Shouldn't happen
-                fault(func_name, 300);
-              }
-              
-            },
-            f_err
-          );
-        };
-        f();
-        
-      } catch (ex) {
-        f_err(ex);
+      },
+      function(reason) {
+        // Failed to load data index over HTTP
+        f_err("Failed to load data index file: " + reason);
       }
-      
-    }, f_err);
-  }
-
-  /*
-   * Load just the data index file to determine the data version string
-   * in the current data file list.
-   * 
-   * This is much faster than the go() operation, so you can use this to
-   * check if you really need to reload the database or whether
-   * everything is already in there.
-   * 
-   * The f_check function is called on success, and it gets a single
-   * string parameter that is the dataver stored in the data file index.
-   * 
-   * The f_err function is called on failure, and takes a reason
-   * parameter.
-   * 
-   * Parameters:
-   * 
-   *   f_check : function - callback that takes a string which will hold
-   *   the dataver read from the data file index
-   * 
-   *   f_err : function - error callback that takes a reason parameter
-   */
-  function checkDataver(f_check, f_err) {
-    
-    var func_name = "checkDataver";
-    
-    // Check parameters
-    if ((typeof(f_check) !== "function") ||
-        (typeof(f_err) !== "function")) {
-      fault(func_name, 100);
-    }
-    
-    // Asynchronously load the index file, function continues in the
-    // callback
-    loadGZJSON(
-        canto_config.data_base + canto_config.index_name,
-        function(js_index) {
-      
-      // Wrap handling in a try-catch that reports to the f_err callback
-      // if there is any exception
-      try {
-      
-        // Parsed JSON should be object
-        if (typeof(js_index) !== "object") {
-          fault(func_name, 200);
-        }
-        
-        // Parsed object should have a "dataver" parameter
-        if (!("dataver" in js_index)) {
-          fault(func_name, 210);
-        }
-      
-        // "dataver" should be a string
-        if (typeof(js_index.dataver) !== "string") {
-          fault(func_name, 220);
-        }
-        
-        // Check dataver format
-        if (!((/^[0-9]{4}-[0-9]{2}-[0-9]{2}:[0-9]{3}$/
-                ).test(js_index.dataver))) {
-          fault(func_name, 230);
-        }
-      
-        // Invoke the callback with the version
-        f_check(js_index.dataver);
-      
-      } catch (ex) {
-        f_err(ex);
-      }
-      
-    }, f_err);
+    );
   }
 
   /*
@@ -399,8 +371,7 @@
    * All exports are declared within a global "ctt_main" object.
    */
   window.ctt_load = {
-    "go": go,
-    "checkDataver": checkDataver
+    "initDB": initDB
   };
 
 }());
